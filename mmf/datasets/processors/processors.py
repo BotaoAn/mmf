ab9ba7b43821e4eddb0fd82166ba41cba6b1d717
Botao An <botaoan.2018@gmail.com>
2022-12-05 02:02:45 -0800

    change processor

--- mmf/datasets/processors/processors.py
+++ mmf/datasets/processors/processors.py
@@ -1778,3 +1778,31 @@ class DETRImageAndTargetProcessor(BaseProcessor):
             raise Exception(f"unknown dataset_type: {dataset_type}")
 
         return {"img": img, "target": target}
+
+# Register the processor so that MMF can discover it
+@registry.register_processor("fasttext_sentence_vector")
+class FastTextSentenceVectorProcessor(FastTextProcessor):
+    # Override the call method
+    def __call__(self, item):
+        # This function is present in FastTextProcessor class and loads
+        # fasttext bin
+        self._load_fasttext_model(self.model_file)
+        if "text" in item:
+            text = item["text"]
+        elif "tokens" in item:
+            text = " ".join(item["tokens"])
+
+        # Get a sentence vector for sentence and convert it to torch tensor
+        sentence_vector = torch.tensor(
+            self.model.get_sentence_vector(text),
+            dtype=torch.float
+        )
+
+        # Return back a dict
+        return {
+            "text": sentence_vector
+        }
+    
+    # Make dataset builder happy, return a random number
+    def get_vocab_size(self):
+        return None
